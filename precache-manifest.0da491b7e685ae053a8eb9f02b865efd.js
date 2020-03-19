56262e616ada280118e81608befb83f8c162ec4c
Kate <kate.kwak@kono.ai>
2020-03-19 18:20:42 +0900

    Updates

---
+++ precache-manifest.0da491b7e685ae053a8eb9f02b865efd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57d0ecd1348d4fa41135a125d80e347b",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "cf7b7f03bafba9e2412b",
+    "url": "/movie_app/static/css/main.8c91889b.chunk.css"
+  },
+  {
+    "revision": "5f45f23c97bbf82f1b91",
+    "url": "/movie_app/static/js/2.13bd5e02.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app/static/js/2.13bd5e02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf7b7f03bafba9e2412b",
+    "url": "/movie_app/static/js/main.05224703.chunk.js"
+  },
+  {
+    "revision": "c145c45984fe2e41280f",
+    "url": "/movie_app/static/js/runtime-main.ffbc15b4.js"
+  }
+]);
